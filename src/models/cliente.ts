373f38e05eb80430a58ca6b96e45e29920b9a5bf
Pablo Russo <pablorussotancara@gmail.com>
2024-12-05 17:53:50 -0400

    Backend Final

--- src/models/cliente.ts
+++ src/models/cliente.ts
@@ -3,7 +3,8 @@ import type { Venta } from './venta'
 export interface Cliente {
   id: number
   idVenta: number
-  nombreApellido: string
+  nombre: string
+  apellido: string
   telefono: string
   email: string
   direccion: string
--- src/models/venta.ts
+++ src/models/venta.ts
@@ -1,14 +1,10 @@
 import type { Cliente } from './cliente'
-import type { DetalleVenta } from './detalleventa'
-import type { Producto } from './producto'
+import type { Usuario } from './usuario'
 
 export interface Venta {
   id: number
   idCliente: number
-  idProducto: number
   fecha: Date
   total: number
   cliente: Cliente
-  producto: Producto
-  detalles: DetalleVenta[]
 }
