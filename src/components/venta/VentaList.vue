2668de244e07434fcc7b2b26512d0d4e1a8322e2
Pablo Russo <pablorussotancara@gmail.com>
2024-12-05 16:03:31 -0400

    Frontend

--- src/components/venta/VentaList.vue
+++ src/components/venta/VentaList.vue
@@ -1,57 +1,48 @@
 <script setup lang="ts">
-import type { DetalleVenta } from '@/models/detalleventa';
-import type { Venta } from '@/models/venta';
-import type { Cliente } from '@/models/cliente';
-import type { Producto } from '@/models/producto';
-import http from '@/plugins/axios';
-import Button from 'primevue/button';
-import Dialog from 'primevue/dialog';
-import { onMounted, ref } from 'vue';
+import type { Venta } from '@/models/venta'
+import http from '@/plugins/axios'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import { onMounted, ref } from 'vue'
 
-const ENDPOINT = 'ventas';
-let ventas = ref<Venta[]>([]);
-let clientes = ref<Cliente[]>([]);
-let productos = ref<Producto[]>([]);
+const ENDPOINT = 'ventas'
+let ventas = ref<Venta[]>([])
+const emit = defineEmits(['edit'])
+const ventaDelete = ref<Venta | null>(null)
+const mostrarConfirmDialog = ref<boolean>(false)
 
-const emit = defineEmits(['edit']);
-const ventaDelete = ref<Venta | null>(null);
-const mostrarConfirmDialog = ref<boolean>(false);
-
-// Obtener la lista de ventas
 async function obtenerLista() {
-  ventas.value = await http.get(ENDPOINT).then((response) => response.data);
-}
-// Mostrar diálogo de confirmación para eliminar
-function mostrarEliminarConfirm(venta: Venta) {
-  ventaDelete.value = venta;
-  mostrarConfirmDialog.value = true;
+  ventas.value = await http.get(ENDPOINT).then(response => response.data)
 }
 
-// Eliminar venta
+function emitirEdicion(venta: Venta) {
+  emit('edit', venta)
+}
+
+function mostrarEliminarConfirm(venta: Venta) {
+  ventaDelete.value = venta
+  mostrarConfirmDialog.value = true
+}
+
 async function eliminar() {
-  try {
-    await http.delete(`${ENDPOINT}/${ventaDelete.value?.id}`);
-    obtenerLista();
-    mostrarConfirmDialog.value = false;
-  } catch (error) {
-    console.error('Error al eliminar la venta:', error);
-  }
+  await http.delete(`${ENDPOINT}/${ventaDelete.value?.id}`)
+  obtenerLista()
+  mostrarConfirmDialog.value = false
 }
 
 onMounted(() => {
-  obtenerLista();
-});
+  obtenerLista()
+})
+defineExpose({ obtenerLista })
 </script>
 
 <template>
   <div>
-    <!-- Listado de ventas -->
     <table>
       <thead>
         <tr>
           <th>Nro.</th>
-          <th>Cliente</th>
-          <th>Detalles</th>
+          <th>Clientes</th>
           <th>Fecha</th>
           <th>Total</th>
           <th>Acciones</th>
@@ -60,29 +51,40 @@ onMounted(() => {
       <tbody>
         <tr v-for="(venta, index) in ventas" :key="venta.id">
           <td>{{ index + 1 }}</td>
-          <td>{{ venta.cliente?.nombreApellido }}</td>
-          <td>
-            <ul>
-              <li v-for="detalle in venta.detalles" :key="detalle.idProducto">
-                {{ detalle.producto?.nombreProducto }} - Cantidad: {{ detalle.cantidad }}
-              </li>
-            </ul>
-          </td>
+          <td>{{ venta.cliente.nombre }} {{ venta.cliente.apellido }}</td>
           <td>{{ venta.fecha }}</td>
           <td>{{ venta.total }}</td>
           <td>
-            <Button icon="pi pi-pencil" aria-label="Editar" text @click="emit('edit', venta)" />
-            <Button icon="pi pi-trash" aria-label="Eliminar" text @click="mostrarEliminarConfirm(venta)" />
+            <Button
+              icon="pi pi-pencil"
+              aria-label="Editar"
+              text
+              @click="emitirEdicion(venta)"
+            />
+            <Button
+              icon="pi pi-trash"
+              aria-label="Eliminar"
+              text
+              @click="mostrarEliminarConfirm(venta)"
+            />
           </td>
         </tr>
       </tbody>
     </table>
 
-    <!-- Confirmación de eliminación -->
-    <Dialog v-model:visible="mostrarConfirmDialog" header="Confirmar Eliminación" :style="{ width: '25rem' }">
+    <Dialog
+      v-model:visible="mostrarConfirmDialog"
+      header="Confirmar Eliminación"
+      :style="{ width: '25rem' }"
+    >
       <p>¿Estás seguro de que deseas eliminar este registro?</p>
       <div class="flex justify-end gap-2">
-        <Button type="button" label="Cancelar" severity="secondary" @click="mostrarConfirmDialog = false" />
+        <Button
+          type="button"
+          label="Cancelar"
+          severity="secondary"
+          @click="mostrarConfirmDialog = false"
+        />
         <Button type="button" label="Eliminar" @click="eliminar" />
       </div>
     </Dialog>
--- src/models/detalleventa.ts
+++ src/models/detalleventa.ts
@@ -1,23 +1,13 @@
 import type { Producto } from './producto'
-import type { Venta } from './venta';
+//import type { Venta } from './venta'
 
 export interface DetalleVenta {
-  id?: number;
-  idProducto: number;
-  idVenta: number;
-  cantidad: number;
-  producto?: {
-    id: number;
-    idCategoria: number;
-    nombreProducto: string;
-    descripcion: string;
-    precio: number;
-    stock: number;
-    categoria: {
-      id: number;
-      descripcion: string;
-    };
-  };
-  venta: Venta
+  id: number
+  //idVenta: number
+  idProducto: number
+  cantidad: number
+  precioUnitario: number
+  subTotal: number
+  producto: Producto
+  //venta: Venta
 }
-
